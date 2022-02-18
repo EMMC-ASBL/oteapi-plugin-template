40849e2bd685224e1e3de763e53ba6f949d6a679
Casper Welzel Andersen <casper.andersen@epfl.ch>
2022-02-18 22:56:36 +0100

    Add `use_git` input
    Whether or not to run the post-hooks script to initialize the generated
    repo for use with `git`.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,6 +7,18 @@
 # [cookiecutter documentation](https://cookiecutter.readthedocs.io/en/latest/advanced/hooks.html#current-working-directory)
 # the current working directory for this script is in the generated project folder.
 
+case {{ cookiecutter.use_git }} in
+    y | Y | yes | Yes | YES | true | True | TRUE | on | On | ON)
+        ;;
+    n | N | no | No | NO | false | False | FALSE | off | Off | OFF)
+        exit
+        ;;
+    *)
+        echo "Did not recognize use_git='{{ cookiecutter.use_git }}' as True/False."
+        echo "Will use the default: True."
+        ;;
+esac
+
 git init
 git add .
 git commit --no-gpg-sign --author "SINTEF <Team4.0@SINTEF.no>" -m "Initial commit
