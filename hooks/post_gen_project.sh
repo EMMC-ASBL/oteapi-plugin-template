773dcf19f1c94b10a4ed2c09764b2aa004c5c6ac
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2022-03-02 09:21:40 +0100

    Add `use_git` input (#36)
    Whether or not to run the post-hooks script to initialize the generated
    repo for use with `git`.
    
    Add `NOT APPLICABLE ` if `use_git` is `False`.
    
    Use `scm_url` throughout (instead of `git_url`).
    
    Update README with updated cookiecutter inputs.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,6 +7,18 @@
 # [cookiecutter documentation](https://cookiecutter.readthedocs.io/en/latest/advanced/hooks.html#current-working-directory)
 # the current working directory for this script is in the generated project folder.
 
+case {{ cookiecutter.use_git }} in
+    y | Y | yes | Yes | YES | true | True | TRUE | on | On | ON)
+        ;;
+    n | N | no | No | NO | false | False | FALSE | off | Off | OFF)
+        exit
+        ;;
+    *)
+        echo "Did not recognize use_git='{{ cookiecutter.use_git }}' as True/False."
+        echo "Will use the default: True."
+        ;;
+esac
+
 git init
 git add .
 git commit --no-gpg-sign --author "SINTEF <Team4.0@SINTEF.no>" -m "Initial commit
@@ -15,5 +27,5 @@ Generated {{ cookiecutter.project_name }} using
 oteapi-plugin-template: A cookiecutter template developed by SINTEF.
 https://github.com/EMMC-ASBL/oteapi-plugin-template
 "
-git remote add origin {{ cookiecutter.git_url }}.git
+git remote add origin {{ cookiecutter.scm_url }}.git
 git branch -M main
