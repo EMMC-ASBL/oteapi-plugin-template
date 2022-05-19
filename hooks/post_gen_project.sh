31a772a4fb9be6eafabfa206fe6e7a23516bf188
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2022-05-19 14:05:59 +0200

    Add GitHub Actions workflows (#72)
    Add extensive information message after having a successful cookiecutter
    repository generation.
    
    Remove the GitHub Actions workflows (and the whole `.github` folder) if
    `use_git` is False.
    
    Remove `git` output in post script.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,6 +11,12 @@ case {{ cookiecutter.use_git }} in
     y | Y | yes | Yes | YES | true | True | TRUE | on | On | ON)
         ;;
     n | N | no | No | NO | false | False | FALSE | off | Off | OFF)
+        rm -rf .github
+        echo "Congratulations on creating '{{ cookiecutter.project_name }}' !
+
+Please write TEAM4.0@SINTEF.no or create issues on GitHub if there are any issues.
+
+Enjoy !"
         exit
         ;;
     *)
@@ -19,13 +25,74 @@ case {{ cookiecutter.use_git }} in
         ;;
 esac
 
-git init
-git add .
+git init >> /dev/null
+git add . >> /dev/null
 git commit --no-gpg-sign --author "SINTEF <Team4.0@SINTEF.no>" -m "Initial commit
 
 Generated {{ cookiecutter.project_name }} using
 oteapi-plugin-template: A cookiecutter template developed by SINTEF.
 https://github.com/EMMC-ASBL/oteapi-plugin-template
-"
-git remote add origin {{ cookiecutter.scm_url }}.git
-git branch -M main
+" >> /dev/null
+git remote add origin {{ cookiecutter.scm_url }}.git >> /dev/null
+git branch -M main >> /dev/null
+
+# Create permanent branch for CI concerning dependency updates through Dependabot.
+git branch ci/dependabot-updates >> /dev/null
+
+# Information message
+echo "Congratulations on creating '{{ cookiecutter.project_name }}' !
+
+For the following commands, it is expected you are in a virtual environment
+with at least Python 3.9.
+
+There are still some last things to do to ensure you have a nice CI/CD setup.
+
+(CI: continuous integration)
+(CD: continuous deployment)
+
+  1. Push 'main' and 'ci/dependabot-updates' branches to the 'origin' remote:
+
+       git push -u origin main:main
+       git push -u origin ci/dependabot-updates:ci/dependabot-updates
+
+  2. Add a 'RELEASE_PAT' secret to GitHub Actions.
+     For more information and documentation on this see:
+
+     https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+
+     https://docs.github.com/en/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-a-repository
+
+  3. Setup branch protection rules for 'main' and 'ci/dependabot-updates'.
+     We recommend to require:
+
+     - Pull requests with at least 1 reviewer approval.
+     - Check success for the CI tests:
+
+       - 'pre-commit
+       - 'pylint-safety'
+       - 'pytest (linux-py3.9)'
+       - 'pytest (windows-py3.9)'
+       - 'Build distribution package'
+       - 'Documentation'
+
+  4. Create a 'CI/CD' label in the GitHub repository.
+
+  5. Start the documentation via GitHub Pages by running:
+
+       # Install the package
+       pip install -U pip setuptools wheel
+       pip install -U -e .[dev]
+
+       # Build and deploy the documentation to the 'gh-pages' branch on GitHub
+       mike deploy --push --config-file mkdocs.yml latest main
+       mike set-default --push --config-file mkdocs.yml latest
+
+  6. Uncomment the CI/CD workflows and other code, where necessary,
+     when you're ready.
+
+  7. Finally, it is VERY IMPORTANT the protection for the 'ci/dependabot-update'
+     branch ONLY allows force pushes for everyone.
+
+Please write TEAM4.0@SINTEF.no or create issues on GitHub if there are any issues.
+
+Enjoy !"
